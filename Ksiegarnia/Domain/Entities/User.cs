0bd90513c3b1890e0282de52137525a5d01b0544
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-11-26 20:11:19 +0100

    Code-First

---
+++ Ksiegarnia/Domain/Entities/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+   
+    public class User
+    {
+        public User(int user_id, string name)
+        {
+            this.user_id=user_id;
+            this.name=name;
+        }
+
+        [Key]
+        public int user_id { get; set; }
+
+        [Required]
+        [MaxLength(45)]
+        public string name { get; set; }
+
+
+        public string postal_code { get; set; }
+        public City City { get; set; }
+
+        [MaxLength(45)]
+        public string lastname { get; set; }
+
+        [MaxLength(45)]
+        public string username { get; set; }
+
+        [MaxLength(45)]
+        public string email { get; set; }
+
+
+        [MaxLength(45)]
+        public string? address { get; set; }
+
+        [MaxLength(45)]
+        public string password { get; set; }
+
+
+
+        public User(int user_id, string name, string postal_code, City city, string lastname, string username, string email, string? address, string password)
+        {
+            this.user_id=user_id;
+            this.name=name;
+            this.postal_code=postal_code;
+            City=city;
+            this.lastname=lastname;
+            this.username=username;
+            this.email=email;
+            this.address=address;
+            this.password=password;
+        }
+
+
+    }
+}
---
+++ Ksiegarnia/Infrastructure/Data/BookStoreContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        public DbSet<City> City { get; set; }
+        public DbSet<User> User { get; set; }
+
+    }
+    public class BloggingContextFactory : IDesignTimeDbContextFactory<Context>
+    {
+        public Context CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Context>();
+            optionsBuilder.UseSqlServer("Data Source=blog.db");
+
+            return new Context(optionsBuilder.Options);
+        }
+    }
+}
