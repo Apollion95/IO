dd1081a2ccdbf0f2a9e39fd34d83cd219869884f
Dominik Katana <56208135+Dominowy@users.noreply.github.com>
2021-11-28 20:22:32 +0100

    Dodanie wydawcy
    Dodanie wydawcy

---
+++ Ksiegarnia/Domain/Entities/Publisher.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Publisher
+    {
+        public int publisher_id { get; set; }
+
+        public string name { get; set;}
+
+        public Publisher(int Id, string Name)
+        {
+            (publisher_id, name) = (Id, Name);
+        }
+    }
+}
