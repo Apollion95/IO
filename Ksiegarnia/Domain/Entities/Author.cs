0851b48956ce37b8475073eafcd63d8ec3550780
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-12-03 18:34:54 +0100

    Add Project

---
+++ Ksiegarnia/Domain/Entities/Author.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class Author
+    {
+        public int author_Id { get; set; }
+
+        public string name { get; set; }
+
+        public string lastName { get; set; }
+
+        public Author(int Id, string Name, string LastName)
+        {
+            (author_Id, name, lastName) = (Id, Name, LastName);
+        }
+    }
+}
---
+++ Ksiegarnia/Domain/Entities/Subscriber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Subscriber
+    {
+        [Key]
+        [Required]
+        public int sub_id { get; set; }
+        [Required]
+        public DateTime sub_start { get; set; }
+        [Required]
+        public int user_id { get; set; }
+        [Required]
+        public DateTime sub_end { get; set; }
+        public bool is_active { get; set; }
+    }
+}
