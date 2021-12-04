cbf58ae76acd0687abc1e77535ba291619ed27ed
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2021-12-04 14:04:55 +0100

    Dodanie index dla strony rejestracji

---
+++ timber/src/components/RegistrationPage/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container, FormWrap, Icon, FormContent, Form, FormInput, FormH1, FormLabel, FormButton } from './LoginInElements' 
+
+const Register = () => {
+  return (
+    <>
+      <Container>
+        <FormWrap>
+          <Icon to='/welcome'>Timber</Icon>
+          <FormContent>
+            <Form action='#'>
+              <FormH1>Załóż swoje konto</FormH1>
+              <FormLabel htmlFor='for'>Email</FormLabel>
+                <FormInput htmlFor='email'/>
+              <FormLabel htmlFor='for'>Hasło</FormLabel>
+                <FormInput htmlFor='password'/>
+              <FormLabel htmlFor='for'>Powtórz Hasło</FormLabel>
+                <FormInput htmlFor='password'/>
+              <FormButton  type='submit'>Zarejestruj się</FormButton>
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
+    </>
+  )
+}
+
+export default Register
