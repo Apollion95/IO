e9f2235d40a2fc1e44bcef0a2608c278acc00bc2
Dominik Katana <hejdominowy@outlook.com>
2021-12-04 14:02:10 +0100

    zmiana struktury index.js

--- timber/src/pages/index.js
+++ timber/src/pages/index.js
@@ -1,19 +1,17 @@
-import React, {useState} from 'react'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import React from 'react'
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () =>{
-    setIsOpen(!isOpen);
-  }
-
   return (
-    <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle}/>
-    </>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <h1>Witaj!</h1>
+    </div>
   )
 }
 
