61fe733c70031cdcb38a695797c571bfabf51abe
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2021-12-04 14:05:52 +0100

    Dodanie indexu strony logowania

---
+++ timber/src/components/LoginPage/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, FormWrap, Icon, FormContent, Form, FormInput, FormH1, FormLabel, FormButton, Text } from './LoginInElements' 
+
+const LoginIn = () => {
+  return (
+    <>
+      <Container>
+        <FormWrap>
+          <Icon to='/welcome'>Timber</Icon>
+          <FormContent>
+            <Form action='#'>
+              <FormH1>Zaloguj się do swojego konta</FormH1>
+              <FormLabel htmlFor='for'>Email</FormLabel>
+                <FormInput htmlFor='email'/>
+              <FormLabel htmlFor='for'>Hasło</FormLabel>
+                <FormInput htmlFor='password'/>
+              <FormButton  type='submit'>Zaloguj Się</FormButton>
+              <Text>Zapomniałeś Hasła?</Text>
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
+    </>
+  )
+}
+
+export default LoginIn
