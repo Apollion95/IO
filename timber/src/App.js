71186c4875eae3f31cad1f66c032d16e4c44acc4
Dominik Katana <hejdominowy@outlook.com>
2021-12-04 13:59:07 +0100

    Zmiana struktury pliku app.js

--- timber/src/App.js
+++ timber/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import './App.css'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
 import Home from './pages'
+import About from './pages/about'
+import Discover from './pages/discover'
+import LoginIn from './components/LoginPage'
+import Register from './components/RegistrationPage'
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () =>{
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <Router>
-      <Home />
-    </Router>
+    <>
+      <Router>
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        <Navbar toggle={toggle}/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/discover' element={<Discover />} />
+          <Route path='/signup' element={<Register />} />
+          <Route path='/signin' element={<LoginIn />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
