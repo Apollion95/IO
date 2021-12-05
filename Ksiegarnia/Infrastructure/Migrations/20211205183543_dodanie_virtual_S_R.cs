607fc1bc762e2b18cb2b53073df9e8573840800d
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-12-05 19:42:10 +0100

    Dodanie migracji

---
+++ Ksiegarnia/Infrastructure/Migrations/20211205183543_dodanie_virtual_S_R.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class dodanie_virtual_S_R : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscribers_user_id",
+                table: "Subscribers",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_book_id",
+                table: "Reviews",
+                column: "book_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_user_id",
+                table: "Reviews",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Book_book_id",
+                table: "Reviews",
+                column: "book_id",
+                principalTable: "Book",
+                principalColumn: "book_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Users_user_id",
+                table: "Reviews",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscribers_Users_user_id",
+                table: "Subscribers",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Book_book_id",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Users_user_id",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscribers_Users_user_id",
+                table: "Subscribers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subscribers_user_id",
+                table: "Subscribers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_book_id",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_user_id",
+                table: "Reviews");
+        }
+    }
+}
