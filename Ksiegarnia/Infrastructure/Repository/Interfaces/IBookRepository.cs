756a0e0a8a3474a21b8ef672a1ead1cce2a2f485
Seba <Sebastian.krzus@gmail.com>
2021-12-05 19:53:10 +0100

    Dodano interfejs IBookRepository

---
+++ Ksiegarnia/Infrastructure/Repository/Interfaces/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Interfaces
+{
+    internal interface IBookRepository : IDisposable
+    {
+        IEnumerable<Book> GetBooks();
+        Book GetBookByName (string name);
+        Book GetBookById (int id);
+        void InsertBook (Book book);
+        void UpdateBook (Book book);
+        void DeleteBook (int id);
+    }
+}
