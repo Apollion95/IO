0865f1f45e42e1e5501b1759e7529d1ffe951f78
Seba <Sebastian.krzus@gmail.com>
2021-12-05 19:49:06 +0100

    Dodano interfejs IUserRepository

---
+++ Ksiegarnia/Infrastructure/Repository/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    internal interface IUserRepository : IDisposable
+    {
+        IEnumerable<User> GetUsers();
+        User GetUserById(int userId);
+        void InsertUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int userId);
+
+    }
+}
