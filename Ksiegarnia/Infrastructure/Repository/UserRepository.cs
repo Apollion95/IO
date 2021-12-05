e075f2bcc6f113499e1a36a3afb18131db5b59df
Seba <Sebastian.krzus@gmail.com>
2021-12-05 19:59:54 +0100

    Dodano klasę UserRepository

---
+++ Ksiegarnia/Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    internal class UserRepository : IUserRepository
+    {
+        public void DeleteUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
