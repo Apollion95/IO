fee33a7707a279040729d24bb2214c01a7af0555
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-12-10 18:03:35 +0100

    Dodanie interfejsu ITransactionsRepository

---
+++ Ksiegarnia/Infrastructure/Repository/Interfaces/ITransactionsRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Interfaces
+{
+    public interface ITransactionsRepository
+    {
+        IEnumerable<Transactions> GetTransactions();
+        Transactions GetById(int id);
+        Transactions GetByUserId(int user_id);
+        Transactions GetByBookName(int book_name);
+        Transactions Add(Transactions transactions);
+        void Update(Transactions transactions);
+        void Delete(Transactions transactions);
+    }
+}
