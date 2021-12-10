6508ffafe076b51ac090da26f8c3375d18d62033
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-12-10 18:02:50 +0100

    Dodanie interfejsu IPublisherRepository

---
+++ Ksiegarnia/Infrastructure/Repository/Interfaces/IPublisherRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Interfaces
+{
+    public interface IPublisherRepository
+    {
+        IEnumerable<Publisher> GetPublisher();
+        Publisher GetByName(string name);
+        Publisher GetById(int id);
+        Publisher Add(Publisher publisher);
+        void Update(Publisher publisher);
+        void Delete(Publisher publisher);
+    }
+}
