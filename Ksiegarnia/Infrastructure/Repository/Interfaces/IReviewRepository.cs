703bc4d0fbabafce478fd025905cf93112081d99
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-12-10 18:03:06 +0100

    Dodanie interfejsu IReviewRepository

---
+++ Ksiegarnia/Infrastructure/Repository/Interfaces/IReviewRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Interfaces
+{
+    public interface IReviewRepository 
+    {
+        IEnumerable<Review> GetReview();
+        Review GetByName(string name);
+        Review GetById(int id);
+        Review GetByRating(int rating);
+        Review Add(Review review);
+        void Update(Review review);
+        void Delete(Review review);
+    }
+}
