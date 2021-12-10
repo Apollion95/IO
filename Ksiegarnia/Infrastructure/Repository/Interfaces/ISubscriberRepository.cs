518af5436ca99e0863741d397f79214335f845c5
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-12-10 18:03:18 +0100

    Dodanie interfejsu ISubscriberRepository

---
+++ Ksiegarnia/Infrastructure/Repository/Interfaces/ISubscriberRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Interfaces
+{
+    public interface ISubscriberRepository
+    {
+        IEnumerable<Subscriber> GetSubscriber();
+        Subscriber GetById(int id);
+        Subscriber GetByUserId(int user_id);
+        Subscriber Add(Subscriber subscriber);
+        void Update(Subscriber subscriber);
+        void Delete(Subscriber subscriber);
+    }
+}
