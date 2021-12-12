d7eaacbffb32274cf52b3811839c14f4bbf21696
Seba <Sebastian.krzus@gmail.com>
2021-12-12 12:02:15 +0100

    Aktualizacje ILibraryRepository

--- Ksiegarnia/Infrastructure/Repository/Interfaces/ILibraryRepository.cs
+++ Ksiegarnia/Infrastructure/Repository/Interfaces/ILibraryRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain.Entities;
+using Microsoft.CodeAnalysis;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repository.Interfaces
 {
-    internal class ILibraryRepository
+    internal interface ILibraryRepository : IDisposable
     {
+        PagedList<Library> GetLibraryByUserId(int pageNumber, int userId);
+        Optional<Library> GetLibrary(int userId, int bookId);
+        void InsertLibrary(int userId, int bookId);
+        void DeleteLibrary(int userId, int bookId);
     }
 }
