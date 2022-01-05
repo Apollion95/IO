cf8b8604a61bc4964a18f0f478e4d107adbe9412
Seba <Sebastian.krzus@gmail.com>
2022-01-05 17:34:00 +0100

    Implementacja testu BookRepository_Should_Insert_Book

--- Ksiegarnia/Bookstore.Tests/RepositoryTests/BookRepositoryTest.cs
+++ Ksiegarnia/Bookstore.Tests/RepositoryTests/BookRepositoryTest.cs
@@ -207,6 +207,52 @@ namespace Bookstore.Tests.RepositoryTests
         }
 
 
+        [Fact]
+        public async Task BookRepository_Should_Insert_Book()
+        {
+            //given
+            var dbOptions = new DbContextOptionsBuilder<BookStoreContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+            BookStoreContext context = new BookStoreContext(dbOptions);
+            context.Authors.Add(new Author(
+              Id: 1,
+              Name: "Adam",
+              LastName: "Mickiewicz"
+              ));
+
+            context.Publishers.Add(new Publisher(
+                Id: 1,
+                Name: "MG"
+            ));
+            context.SaveChanges();
+
+            BookRepository bookRepository = new BookRepository(context);
+            Book book = new Book();
+            book.book_id = 1;
+            book.name = "Pan Tadeusz";
+            book.releaseDate = new DateTime(2002, 07, 16);
+            book.author = context.Authors.Find(1);
+            book.publisher = context.Publishers.Find(1);
+            //when
+            bookRepository.InsertBook(book);
+            Optional<Book> bookFromDb = bookRepository.GetBookById(1);
+
+            //then
+            Assert.True(bookFromDb.HasValue);
+            Assert.Equal(1, bookFromDb.Value.book_id);
+            Assert.Equal("Pan Tadeusz", bookFromDb.Value.name);
+            Assert.Equal(new DateTime(2002, 07, 16), bookFromDb.Value.releaseDate);
+
+            Assert.Equal(1, bookFromDb.Value.author.author_Id);
+            Assert.Equal("Adam", bookFromDb.Value.author.name);
+            Assert.Equal("Mickiewicz", bookFromDb.Value.author.lastName);
+
+            Assert.Equal(1, bookFromDb.Value.publisher.publisher_id);
+            Assert.Equal("MG", bookFromDb.Value.publisher.name);
+
+        }
 
 
     }
