94a762efc77038b192f3ba58b6a45812479e81ca
Seba <Sebastian.krzus@gmail.com>
2022-01-09 18:45:37 +0100

    Implementacja testu BookRepository_Should_Get_Books_By_Publisher

--- Ksiegarnia/Bookstore.Tests/RepositoryTests/BookRepositoryTest.cs
+++ Ksiegarnia/Bookstore.Tests/RepositoryTests/BookRepositoryTest.cs
@@ -354,5 +354,64 @@ namespace Bookstore.Tests.RepositoryTests
             Assert.NotNull(books[1]);
 
         }
+
+
+
+        [Fact]
+        public async Task BookRepository_Should_Get_Books_By_Publisher()
+        {
+            //given
+            var dbOptions = new DbContextOptionsBuilder<BookStoreContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+            BookStoreContext context = new BookStoreContext(dbOptions);
+            context.Authors.Add(new Author(
+              Id: 1,
+              Name: "Adam",
+              LastName: "Mickiewicz"
+              ));
+
+            context.Authors.Add(new Author(
+             Id: 2,
+             Name: "Krystian",
+             LastName: "Lipiński"
+             ));
+
+            context.Publishers.Add(new Publisher(
+                Id: 1,
+                Name: "MG"
+            ));
+
+            Book book = new Book();
+            book.book_id = 1;
+            book.name = "Pan Tadeusz";
+            book.releaseDate = new DateTime(2002, 07, 16);
+            book.author = context.Authors.Find(1);
+            book.publisher = context.Publishers.Find(1);
+            context.Store.Add(book);
+
+            Book book2 = new Book();
+            book2.book_id = 2;
+            book2.name = "Pan Adam";
+            book2.releaseDate = new DateTime(1997, 10, 07);
+            book2.author = context.Authors.Find(2);
+            book2.publisher = context.Publishers.Find(1);
+            context.Store.Add(book2);
+
+            context.SaveChanges();
+
+            BookRepository bookRepository = new BookRepository(context);
+
+
+            //when
+            List<Book> books = bookRepository.GetBooksByPublisher(1, "MG").ToList();
+
+            //then
+            Assert.Equal(2, books.Count);
+            Assert.NotNull(books[0]);
+            Assert.NotNull(books[1]);
+
+        }
     }
 }
