cf31033b6d6fc49fa024872213e9d8622005fd13
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2022-01-09 19:30:27 +0100

    Przygotowanie logowania

--- Ksiegarnia/Ksiegarnia/Controllers/HomeController.cs
+++ Ksiegarnia/Ksiegarnia/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Infrastructure.Repository;
+using Ksiegarnia.Dtos;
 using Ksiegarnia.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
 
 
 namespace Ksiegarnia.Controllers
@@ -16,6 +21,24 @@ namespace Ksiegarnia.Controllers
         {
             _repository = repository;
             _jwtService = jwtService;
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto dto)
+        {
+            var user = _repository.GetUserByEmail(dto.Email);
+
+            if (user == null) return BadRequest(new { message = "Nie poprawne dane" });
+
+            if (!BCrypt.Net.BCrypt.Verify(dto.Password, user.Password))
+            {
+                return BadRequest(new { message = "Nie poprawne dane" });
+            }
+
+            var jwt = _jwtService.Generate(user.Id);
+
+            Response.Cookies.Append("jwt", jwt, new CookieOptions { HttpOnly = true });
+
+            return Ok(new { message = "succes" });
         }
 
     }
