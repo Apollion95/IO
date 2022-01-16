573c03e03a516ffb8c295970dd99816e532ea67a
Seba <Sebastian.krzus@gmail.com>
2022-01-16 19:04:11 +0100

    Dodanie AuthorDto

---
+++ Ksiegarnia/Ksiegarnia/Dtos/AuthorDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class AuthorDto
+    {
+        public int author_Id { get; set; }
+        
+        public string name { get; set; }
+      
+        public string lastName { get; set; }
+
+        public AuthorDto(int id, string name, string lastName)
+        {
+            author_Id = id;
+            this.name = name;
+            this.lastName = lastName;
+        }
+    }
+}
