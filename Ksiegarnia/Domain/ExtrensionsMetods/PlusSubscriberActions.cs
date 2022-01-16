7273e300818e88bb0f25f568d22e435dde176d31
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-16 13:03:36 +0100

    Create PlusSubscriberActions.cs

---
+++ Ksiegarnia/Domain/ExtrensionsMetods/PlusSubscriberActions.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ExtrensionsMetods
+{
+    public class PlusSubscriberActions : IActions
+    {
+        public void Read()
+        {
+            //czytanie bez limitu
+        }
+
+
+
+
+    }
+}
