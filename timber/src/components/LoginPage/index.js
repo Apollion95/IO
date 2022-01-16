0f38511e5641727a88a0a2454777a3beb2d88dc8
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2022-01-16 15:50:24 +0100

    dodanie stanów password i navigate

--- timber/src/components/LoginPage/index.js
+++ timber/src/components/LoginPage/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
 import { Container, FormWrap, Icon, FormContent, Form, FormInput, FormH1, FormLabel, FormButton, Text } from './LoginInElements' 
 import React, {useState} from 'react'
 
 const Login = (props) => {
 const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [navigate, setNavigate] = useState('');
 
 
 const LoginIn = () => {
