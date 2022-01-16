add93c4673582add9eca6c4a48fbb1e302326389
Seba <Sebastian.krzus@gmail.com>
2022-01-16 21:16:31 +0100

    Dodanie AuthorMappera

---
+++ Ksiegarnia/Ksiegarnia/Mapper/AuthorMapper.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Ksiegarnia.Dtos;
+
+namespace Ksiegarnia.Mapper
+{
+    public class AuthorMapper
+    {
+        private Author convertToEntity(AuthorDto dto)
+        {
+            return  new Author(dto.author_Id, dto.name, dto.lastName);        
+        }
+
+
+    }
+}
