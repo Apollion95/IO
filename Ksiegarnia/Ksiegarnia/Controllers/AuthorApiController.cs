9d4d2c16717d8b185b16a9b8aba13361009113cd
Seba <Sebastian.krzus@gmail.com>
2022-01-16 17:27:36 +0100

    Dodanie AuthorApiController

---
+++ Ksiegarnia/Ksiegarnia/Controllers/AuthorApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ksiegarnia.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class AuthorApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
