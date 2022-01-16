2ad5c0c21e466d2a99fa526b1bb2e880a15bd6ed
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-16 13:03:39 +0100

    Create RegularSubscriberActions.cs

---
+++ Ksiegarnia/Domain/ExtrensionsMetods/RegularSubscriberActions.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ExtrensionsMetods
+{
+    public class RegularSubscriberActions : IActions
+    {
+
+        public int PosiblePagesToRead { get; set; }
+
+        public RegularSubscriberActions()
+        {
+            PosiblePagesToRead = 20;
+        }
+        public void Read()
+        {
+            if (PosiblePagesToRead == 0)
+            {
+                throw new Exception("You can't read any more pages");
+            }
+            else
+            {
+                PosiblePagesToRead--;
+            }
+            
+        }
+
+
+
+       
+    }
+}
