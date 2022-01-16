6fee6942bc759bac7213d2457d155a12ea7d98ca
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-16 13:03:34 +0100

    Create Test.cs

---
+++ Ksiegarnia/Domain/Extensions/Test.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.ExtrensionsMetods;
+using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Extensions
+{
+    public class Test
+    {
+        public void Tests()
+        {
+            var premium = new RegularSubscriberActions();
+            
+            var a = new PlusSubscriber(3,DateTime.Now,new User(), DateTime.Now+TimeSpan.FromHours(6),true, premium);
+            a.Read();
+            a.FreeBook();
+            a.TakeFreeBook();
+            
+        }
+    }
+}
