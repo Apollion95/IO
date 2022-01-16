7dc1845eeed9869da34ae112f2d6a5b837911262
Seba <Sebastian.krzus@gmail.com>
2022-01-16 21:29:34 +0100

    Modyfikacja API o korzystanie z obiektu typu DTO

--- Ksiegarnia/Ksiegarnia/Controllers/AuthorApiController.cs
+++ Ksiegarnia/Ksiegarnia/Controllers/AuthorApiController.cs
@@ -1,28 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Infrastructure.Repository.Interfaces;
+using Ksiegarnia.Mapper;
+
 namespace Ksiegarnia.Controllers
 {
     [ApiController]
-    [Route("author")]
+    [Route("api/author")]
     [Produces("application/json")]
     public class AuthorApiController : Controller
     {
         public IAuthorRepository authorRepository;
-       public AuthorApiController(IAuthorRepository authorRepository)
+        public AuthorMapper authorMapper;
+        public AuthorApiController(IAuthorRepository authorRepository, AuthorMapper authorMapper)
         {
             this.authorRepository = authorRepository;
+            this.authorMapper = authorMapper;
         }
 
         [HttpGet("{id}")]
         public JsonResult GetById(int id)
         {
+
             var author = authorRepository.GetAuthorById(id);
             if (author.HasValue)
-                return new JsonResult(author);
+            {
+                var authorDto = authorMapper.convertToDto(author.Value);
+                return new JsonResult(authorDto);
+
+            }
             return new JsonResult(null);
         }
 
-        [HttpGet("{}")]
+
         
 
     }
--- Ksiegarnia/Ksiegarnia/Mapper/AuthorMapper.cs
+++ Ksiegarnia/Ksiegarnia/Mapper/AuthorMapper.cs
@@ -5,12 +5,12 @@ namespace Ksiegarnia.Mapper
 {
     public class AuthorMapper
     {
-        private Author convertToEntity(AuthorDto dto)
+        public Author convertToEntity(AuthorDto dto)
         {
             return  new Author(dto.author_Id, dto.name, dto.lastName);        
         }
 
-        private AuthorDto convertToDto(Author entity)
+        public AuthorDto convertToDto(Author entity)
         {
             return new AuthorDto(entity.author_Id, entity.name, entity.lastName);
         }
