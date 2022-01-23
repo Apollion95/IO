1a9d0bfc50ac3025ba604219799c42b9724eaa60
Dominik Katana <hejdominowy@outlook.com>
2022-01-23 15:10:03 +0100

    Przygotowanie szaqblonu podstrony

---
+++ timber/src/pages/book.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect, useMemo} from 'react';
+
+
+
+const Book = () => {
+    const [Books, setBooks] = useState([]);
+
+    useEffect(() => {
+        
+    }, []);
+}
+
+export default Book;
