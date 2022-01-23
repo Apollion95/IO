c9c7059e36dee1e035776a533f72bd56cc1ef878
Seba <sebastian.krzus@gmail.com>
2022-01-23 17:20:15 +0100

    Dodanie projektu API wraz z kontrolerem(AuthorApiController)

---
+++ Ksiegarnia/Bookstore/Controllers/AuthorApiController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ksiegarnia.Mapper;
+using Domain.Entities;
+using System.Diagnostics;
+using Infrastructure.Repository.Interfaces;
+
+namespace Ksiegarnia.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    [Produces("application/json")]
+    public class AuthorApiController : Controller
+    {
+        public IAuthorRepository authorRepository;
+        public AuthorMapper authorMapper;
+        public AuthorApiController(IAuthorRepository authorRepository)
+        {
+            this.authorRepository = authorRepository;
+            
+        }
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            return new JsonResult(new Author(1,"Adam", "Mickiewicz"));
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetById(int id)
+        {
+
+            var author = authorRepository.GetAuthorById(id);
+            if (author.HasValue)
+            {
+                //var authorDto = authorMapper.convertToDto(author.Value);
+                return new JsonResult(author);
+
+            }
+            return new JsonResult(null);
+        }
+
+        [HttpGet("{id}/books")]
+        public JsonResult GetAuthorBooks(int id)
+        {
+
+            var author = authorRepository.GetAuthorById(id);
+            if (author.HasValue)
+            {
+                List<Book> books = (List<Book>)author.Value.books;
+                return new JsonResult(books);
+
+            }
+            return new JsonResult(null);
+        }
+
+
+
+
+    }
+}
---
+++ Ksiegarnia/Bookstore/Program.cs
@@ -0,0 +1,50 @@
+using Infrastructure.Data;
+using Pomelo.EntityFrameworkCore.MySql;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Repository.Interfaces;
+using Infrastructure.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddCors();
+builder.Services.AddDbContext<BookStoreContext>(options =>
+{
+
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 27)));
+
+});
+
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
