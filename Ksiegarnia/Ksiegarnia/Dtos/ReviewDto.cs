00c7f1a74b872733e4e2fac2393f7dcdb358c2f9
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-23 16:48:19 +0100

    Create ReviewDto.cs

---
+++ Ksiegarnia/Ksiegarnia/Dtos/ReviewDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class ReviewDto
+    {
+      
+        public int review_id { get; set; }
+        public string review { get; set; }
+    
+        public int rating { get; set; }
+   
+        public virtual BookDto book { get; set; }
+  
+        public virtual UserDto user { get; set; }
+    }
+}
