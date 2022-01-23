aa3b67f07b6b0f0b9fab746fff99842a5eefbd90
Dominik Katana <hejdominowy@outlook.com>
2022-01-23 15:10:56 +0100

    Fetch pobierajcy dane z api

--- timber/src/pages/book.js
+++ timber/src/pages/book.js
@@ -6,7 +6,13 @@ const Book = () => {
     const [Books, setBooks] = useState([]);
 
     useEffect(() => {
-        
+        getData();
+
+        async function getData() {
+            const response = await fetch("http://localhost:8000/book",{
+            credentials: 'include'});
+            const data = await response.json();
+        }
     }, []);
 }
 
