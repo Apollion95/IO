73eb09c3bb48efa7a0c0de631ed3cad43c108653
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-23 16:48:10 +0100

    Create BookDto.cs

---
+++ Ksiegarnia/Ksiegarnia/Dtos/BookDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class BookDto
+    {
+   
+        public int book_id { get; set; }
+    
+        public string name { get; set; }
+       
+        public DateTime releaseDate { get; set; }
+     
+        public virtual AuthorDto author { get; set; }
+       
+        public virtual PublisherDto publisher { get; set; }
+
+        public virtual ICollection<TransactionsDto> transactions { get; set; }
+
+        public BookDto(int Id, string Name, DateTime ReleaseDate)
+        {
+            (book_id, name, releaseDate) = (Id, Name, ReleaseDate);
+        }
+    }
+}
