ff2ae20f7de283324d4a64fece68c75093bab080
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-23 16:48:17 +0100

    Create PublisherDto.cs

---
+++ Ksiegarnia/Ksiegarnia/Dtos/PublisherDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class PublisherDto
+    {
+  
+        public int publisher_id { get; set; }
+  
+        public string name { get; set; }
+
+
+        public PublisherDto(int Id, string Name)
+        {
+            (publisher_id, name) = (Id, Name);
+        }
+    }
+}
