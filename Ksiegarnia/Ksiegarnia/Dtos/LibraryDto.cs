8f115121d98bfbc52694c8622571b55c80d405bf
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-23 16:48:15 +0100

    Create LibraryDto.cs

---
+++ Ksiegarnia/Ksiegarnia/Dtos/LibraryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class LibraryDto
+    {
+
+        public int library_id { get; set; }
+
+
+        public virtual UserDto user { get; set; }
+
+        public virtual BookDto book { get; set; }
+    }
+}
