1a251d6b5c388aa90527ef9972ed63778912bbba
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-23 16:48:23 +0100

    Create TransactionsDto.cs

---
+++ Ksiegarnia/Ksiegarnia/Dtos/TransactionsDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class TransactionsDto
+    {
+        public TransactionsDto(int trans_id, DateTime date, double price, UserDto user, BookDto book)
+        {
+            this.trans_id=trans_id;
+            this.date=date;
+            this.price=price;
+            this.user=user;
+            this.book=book;
+        }
+
+     
+        public int trans_id { get; set; }
+
+        public DateTime date { get; set; }
+
+        public Double price { get; set; }
+
+        public virtual UserDto user { get; set; }
+
+        public virtual BookDto book { get; set; }
+    }
+}
