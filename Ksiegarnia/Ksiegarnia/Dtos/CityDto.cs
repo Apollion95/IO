d82f13c85d5455c073cf09d777b5f5306cfcb119
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-23 16:48:13 +0100

    Create CityDto.cs

---
+++ Ksiegarnia/Ksiegarnia/Dtos/CityDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class CityDto
+    {
+
+
+        public string postal_code { get; set; }
+
+        public string city { get; set; }
+
+        public CityDto(string postal_code, string city)
+        {
+            this.postal_code=postal_code;
+            this.city=city;
+        }
+
+    }
+}
