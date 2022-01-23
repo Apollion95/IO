b4e5df91a4f9305c3c91dc2d809745ff3d6394f5
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-23 16:48:21 +0100

    Create SubscriberDto.cs

---
+++ Ksiegarnia/Ksiegarnia/Dtos/SubscriberDto.cs
@@ -0,0 +1,26 @@
+﻿namespace Ksiegarnia.Dtos
+{
+    public class SubscriberDto
+    {
+        public SubscriberDto(int sub_id, DateTime sub_start, UserDto user, DateTime sub_end, bool is_active)
+        {
+            this.sub_id=sub_id;
+            this.sub_start=sub_start;
+            this.sub_end=sub_end;
+            this.is_active=is_active;
+            this.user.user_id=user.user_id;
+        }
+ 
+
+
+        public int sub_id { get; set; }
+
+        public DateTime sub_start { get; set; }
+
+        public virtual UserDto user { get; set; }
+
+        public DateTime sub_end { get; set; }
+        public bool is_active { get; set; }
+
+    }
+}
